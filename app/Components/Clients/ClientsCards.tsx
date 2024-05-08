dd4f0c624a1f58514a71ac4ddbee1a7b4935733f
kishan0779 <124876418+kishan0779@users.noreply.github.com>
2024-05-08 22:35:15 +0530

    d

--- app/Components/Clients/ClientsCards.tsx
+++ app/Components/Clients/ClientsCards.tsx
@@ -16,7 +16,7 @@ function ClientsCards({...v}: Ccard) {
             <div className="flex justify-between">
                 <div className=" flex flex-col gap-5">
 
-                    <div className=" w-[375px] h-[510px] sm:h-[517px] sm:w-[384px] border-8 relative " >
+                    <div className=" w-[371px] h-[510px] sm:h-[517px] sm:w-[384px] border-8 relative " >
                         <Image
                             layout='fill'
                             src={v.src}
